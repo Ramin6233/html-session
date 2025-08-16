f5c6add8e90d75bc3d867eb34cdf47200b0aae0b
Ramin6233 <raminkhan@gmx.de>
2025-08-16 13:58:41 +0100

    python first class

---
+++ Python/first program.py
@@ -0,0 +1,7 @@
+print("hello world")
+
+print("Hello, my name is Ramin.")
+print("I am 16 years old.")
+print("I live in germany.")
+print("I am in 11th grade and I go to albert einstein school.")
+print("I like playing basketball.")
