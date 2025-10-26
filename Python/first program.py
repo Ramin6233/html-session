4fc18cb6fd6900a31a49364ead5f0fe381fc171a
Ramin6233 <raminkhan@gmx.de>
2025-10-26 14:42:13 +0100

    code

--- Python/first program.py
+++ Python/first program.py
@@ -5,3 +5,58 @@ print("I am 16 years old.")
 print("I live in germany.")
 print("I am in 11th grade and I go to albert einstein school.")
 print("I like playing basketball.")
+
+
+my_variable=10
+print(my_variable)
+print(type(my_variable))
+
+my_float_variable=12.4
+print(my_float_variable)
+print(type(my_float_variable))
+
+my_strin="Ramin"
+print(my_string)
+print(type(my_string))
+print(int(my_float_variable))
+
+a=5
+b=6
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+
+x=2
+y=3
+print(x**y)
+
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+
+first_name="Ramin"
+last_name="Khan"
+print(first_name+" "+last_name)
+
+print(first_name[0])
+print(first_name[1])
+print(first_name[-1])
+print(first_name[0:3])
+print(first_name[0:])
+print(first_name[:3])
+
+x=input("Enter the value of x:")
+y=input("Enter the value of y:")
+
+temp=x
+x=y
+y=temp
+print("After swapping the value of x is:",x)
+print("After swapping the value of y is:",y)
