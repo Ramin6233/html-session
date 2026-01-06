41e7831e63804b7357d3f42c54bc1ab19181906c
Ramin6233 <raminkhan@gmx.de>
2026-01-06 14:27:26 +0100

    polymorphsm

--- Python/OOP2.py
+++ Python/OOP2.py
@@ -87,3 +87,19 @@ if __name__ == "__main__":
 
         else:
             print("❌ Invalid choice! Please enter a number between 1 and 5.")
+
+
+class Animal(ABC):
+    def move(self):
+        pass
+    class Human(Animal):
+        def move(self):
+            print("I can walk and run")
+        class Snake(Animal):
+            def move(self):
+                print("I can crawl")
+
+R = Human()
+R.move()
+K = Snake()
+K.move()
